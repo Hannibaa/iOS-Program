99c8aa5038ef69d3827b3bd951201c8b6259e3ae
Aoues Kadda <haoues_k@yahoo.fr>
2024-02-02 15:29:17 -0800

    Initial Commit

---
+++ Xcode tutorial Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Xcode tutorial Demo
+//
+//  Created by Aoues Kadda on 2/2/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Xcode tutorial Demo/Xcode_tutorial_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_tutorial_DemoApp.swift
+//  Xcode tutorial Demo
+//
+//  Created by Aoues Kadda on 2/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_tutorial_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
